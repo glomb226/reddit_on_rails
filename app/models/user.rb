9e0c8ca2ed85188186eccc3c2208c98bb1fe75d9
luis martinez <LuisMtzJr@luiss-MacBook-Air.local>
2014-12-16 14:57:15 -0600

    Deise loginout complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :username
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+         attr_accessible :username, :email, :password, :password_confirmation, :remember_me
 end
